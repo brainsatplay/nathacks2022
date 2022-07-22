fbfb49b250d60fa094ad748e28252ccc79cc0ece
Garrett Michael Flynn <46533749+GarrettMFlynn@users.noreply.github.com>
2022-07-21 17:20:33 -0700

    Fixed

--- nathacks_2022/app/code/template/index.js
+++ nathacks_2022/app/code/template/index.js
@@ -1,16 +1,25 @@
-// import button from "./plugins/button/index.js"
-// import data from "./plugins/data/index.js"
+// import datastreams from "./plugins/datastreams/index.js"
+// import ui from "./plugins/ui/index.js"
+
+// Devices
+// import synthetic from "./plugins/devices/synthetic/index.js"
+// import ganglion from "./plugins/devices/ganglion/index.js"
+// import muse from "./plugins/devices/muse/index.js"
+
 
 import pkg from './package.json' assert {type: 'json'};
 // import graph from './.brainsatplay/index.graph.json' assert {type: 'json'};
 
-const ui = {
-    // button,
-    // data,
+export default {
+    // datastreams, 
+    // ui,
+
+    // synthetic,
+    // ganglion,
+    // muse,
+
     ['.brainsatplay']: {
         package: pkg,
-        graph: {}
+        // graph,
     }
 }
-
-export default ui
